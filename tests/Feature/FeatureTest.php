a7994591fa348c1bd4fd334211242351f546fe11
Gaan123 <Gaan123@users.noreply.github.com>
2023-04-16 10:03:54 +0000

    Fix styling

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -1,23 +1,15 @@
 <?php
 
-
 namespace Dada\AutoClassHelper\Tests\Feature;
 
-use Dada\AutoClassHelper\Attributes\ClassLoader;
-use Dada\AutoClassHelper\AutoClassHelper;
-use Dada\AutoClassHelper\Dev\AbstractClass\DynClass;
 use Dada\AutoClassHelper\Dev\Providers\TestProviders;
 use Dada\AutoClassHelper\Tests\TestCase;
 
 class FeatureTest extends TestCase
 {
-
     public function test_get_all_files()
     {
-        $this->assertEquals(1,1);
+        $this->assertEquals(1, 1);
 //        (new TestProviders())->boot();
     }
 }
-
-
-
